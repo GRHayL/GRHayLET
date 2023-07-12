3c2001fa06b793cfa717cbdfd57fe39fa9db2dbb
Samuel Cupp <scupp1@my.apsu.edu>
2023-07-12 16:37:54 -0700

    GRHayLHD: fixed bug in outer bcs; updated test

--- GRHayLHD/src/outer_boundaries.c
+++ GRHayLHD/src/outer_boundaries.c
@@ -11,7 +11,7 @@
 
 #include "GRHayLHD.h"
 
-void GRHayLHD_enforce_primitive_limits_and_compute_conservs(const cGH* cctkGH, const int index);
+void GRHayLHD_enforce_primitive_limits_and_compute_conservs(const cGH* cctkGH, const int index, primitive_quantities prims);
 
 /*******************************************************
  * Apply outer boundary conditions on {P,rho_b,vx,vy,vz}
@@ -26,12 +26,14 @@ void GRHayLHD_outer_boundaries_on_P_rho_b_vx_vy_vz(CCTK_ARGUMENTS) {
 
   if(CCTK_EQUALS(Matter_BC,"frozen")) return;
 
-  bool Symmetry_none=false; if(CCTK_EQUALS(Symmetry,"none")) Symmetry_none=true;
-
-  int levelnumber = GetRefinementLevel(cctkGH);
+  const bool Symmetry_none = CCTK_EQUALS(Symmetry,"none") ? true : false;
 
   // Don't apply approximate outer boundary conditions on initial data, which should be defined everywhere, or on levels != [coarsest level].
-  if(cctk_iteration==0 || levelnumber!=0) return;
+  if(cctk_iteration==0 || GetRefinementLevel(cctkGH)!=0) return;
+
+  const double poison = 0.0/0.0;
+  double dummy1, dummy2, dummy3;
+  double dummy4, dummy5, dummy6;
 
   if(cctk_nghostzones[0]!=cctk_nghostzones[1] || cctk_nghostzones[0]!=cctk_nghostzones[2])
     CCTK_VERROR("ERROR: GRHayLHD outer BC driver does not support unequal number of ghostzones in different directions!");
@@ -44,15 +46,25 @@ void GRHayLHD_outer_boundaries_on_P_rho_b_vx_vy_vz(CCTK_ARGUMENTS) {
 #pragma omp parallel for
       for(int k=0; k<cctk_lsh[2]; k++) {
         for(int j=0; j<cctk_lsh[1]; j++) {
-          const int indm1 = CCTK_GFINDEX3D(cctkGH,imax-1, j, k);
           const int index = CCTK_GFINDEX3D(cctkGH,imax, j, k);
-          pressure[index] = pressure[indm1];
-          rho_b[index]    = rho_b[indm1];
-          vx[index]       = vx[indm1];
-          vy[index]       = vy[indm1];
-          vz[index]       = vz[indm1]; 
-          if(vx[index]<0.) vx[index] = 0.0;
-          GRHayLHD_enforce_primitive_limits_and_compute_conservs(cctkGH, index);
+          const int indm1 = CCTK_GFINDEX3D(cctkGH,imax-1, j, k);
+
+          const double vtmp = vx[indm1] < 0.0 ? 0 : vx[indm1];
+          primitive_quantities prims;
+          ghl_initialize_primitives(
+                rho_b[indm1], pressure[indm1], eps[indm1],
+                vtmp, vy[indm1], vz[indm1],
+                0.0, 0.0, 0.0,
+                poison, poison, poison, &prims);
+
+          GRHayLHD_enforce_primitive_limits_and_compute_conservs(cctkGH, index, prims);
+
+          ghl_return_primitives(
+                &prims,
+                &rho_b[index], &pressure[index], &eps[index],
+                &vx[index], &vy[index], &vz[index],
+                &dummy1, &dummy2, &dummy3,
+                &dummy4, &dummy5, &dummy6);
         }
       }
     }
@@ -64,13 +76,23 @@ void GRHayLHD_outer_boundaries_on_P_rho_b_vx_vy_vz(CCTK_ARGUMENTS) {
         for(int j=0; j<cctk_lsh[1]; j++) {
           const int index = CCTK_GFINDEX3D(cctkGH, imin, j, k);
           const int indp1 = CCTK_GFINDEX3D(cctkGH, imin+1, j, k);
-          pressure[index] = pressure[indp1];
-          rho_b[index]    = rho_b[indp1];
-          vx[index]       = vx[indp1];
-          vy[index]       = vy[indp1];
-          vz[index]       = vz[indp1]; 
-          if(vx[index]>0.) vx[index] = 0.0;
-          GRHayLHD_enforce_primitive_limits_and_compute_conservs(cctkGH, index);
+
+          const double vtmp = vx[indp1] > 0.0 ? 0 : vx[indp1];
+          primitive_quantities prims;
+          ghl_initialize_primitives(
+                rho_b[indp1], pressure[indp1], eps[indp1],
+                vtmp, vy[indp1], vz[indp1],
+                0.0, 0.0, 0.0,
+                poison, poison, poison, &prims);
+
+          GRHayLHD_enforce_primitive_limits_and_compute_conservs(cctkGH, index, prims);
+
+          ghl_return_primitives(
+                &prims,
+                &rho_b[index], &pressure[index], &eps[index],
+                &vx[index], &vy[index], &vz[index],
+                &dummy1, &dummy2, &dummy3,
+                &dummy4, &dummy5, &dummy6);
         }
       }
     }
@@ -82,15 +104,25 @@ void GRHayLHD_outer_boundaries_on_P_rho_b_vx_vy_vz(CCTK_ARGUMENTS) {
 #pragma omp parallel for
       for(int k=0; k<cctk_lsh[2]; k++) {
         for(int i=0; i<cctk_lsh[0]; i++) {
-          const int indm1 = CCTK_GFINDEX3D(cctkGH, i, jmax-1, k);
           const int index = CCTK_GFINDEX3D(cctkGH, i, jmax, k);
-          pressure[index] = pressure[indm1];
-          rho_b[index]    = rho_b[indm1];
-          vx[index]       = vx[indm1];
-          vy[index]       = vy[indm1];
-          vz[index]       = vz[indm1]; 
-          if(vx[index]<0.) vx[index] = 0.0;
-          GRHayLHD_enforce_primitive_limits_and_compute_conservs(cctkGH, index);
+          const int indm1 = CCTK_GFINDEX3D(cctkGH, i, jmax-1, k);
+
+          const double vtmp = vy[indm1] < 0.0 ? 0 : vy[indm1];
+          primitive_quantities prims;
+          ghl_initialize_primitives(
+                rho_b[indm1], pressure[indm1], eps[indm1],
+                vx[indm1], vtmp, vz[indm1],
+                0.0, 0.0, 0.0,
+                poison, poison, poison, &prims);
+
+          GRHayLHD_enforce_primitive_limits_and_compute_conservs(cctkGH, index, prims);
+
+          ghl_return_primitives(
+                &prims,
+                &rho_b[index], &pressure[index], &eps[index],
+                &vx[index], &vy[index], &vz[index],
+                &dummy1, &dummy2, &dummy3,
+                &dummy4, &dummy5, &dummy6);
         }
       }
     }
@@ -102,13 +134,23 @@ void GRHayLHD_outer_boundaries_on_P_rho_b_vx_vy_vz(CCTK_ARGUMENTS) {
         for(int i=0; i<cctk_lsh[0]; i++) {
           const int index = CCTK_GFINDEX3D(cctkGH, i, jmin, k);
           const int indp1 = CCTK_GFINDEX3D(cctkGH, i, jmin+1, k);
-          pressure[index] = pressure[indp1];
-          rho_b[index]    = rho_b[indp1];
-          vx[index]       = vx[indp1];
-          vy[index]       = vy[indp1];
-          vz[index]       = vz[indp1]; 
-          if(vx[index]>0.) vx[index] = 0.0;
-          GRHayLHD_enforce_primitive_limits_and_compute_conservs(cctkGH, index);
+
+          const double vtmp = vy[indp1] > 0.0 ? 0 : vy[indp1];
+          primitive_quantities prims;
+          ghl_initialize_primitives(
+                rho_b[indp1], pressure[indp1], eps[indp1],
+                vx[indp1], vtmp, vz[indp1],
+                0.0, 0.0, 0.0,
+                poison, poison, poison, &prims);
+
+          GRHayLHD_enforce_primitive_limits_and_compute_conservs(cctkGH, index, prims);
+
+          ghl_return_primitives(
+                &prims,
+                &rho_b[index], &pressure[index], &eps[index],
+                &vx[index], &vy[index], &vz[index],
+                &dummy1, &dummy2, &dummy3,
+                &dummy4, &dummy5, &dummy6);
         }
       }
     }
@@ -120,15 +162,25 @@ void GRHayLHD_outer_boundaries_on_P_rho_b_vx_vy_vz(CCTK_ARGUMENTS) {
 #pragma omp parallel for
       for(int j=0; j<cctk_lsh[1]; j++) {
         for(int i=0; i<cctk_lsh[0]; i++) {
-          const int indm1 = CCTK_GFINDEX3D(cctkGH, i, j, kmax-1);
           const int index = CCTK_GFINDEX3D(cctkGH, i, j, kmax);
-          pressure[index] = pressure[indm1];
-          rho_b[index]    = rho_b[indm1];
-          vx[index]       = vx[indm1];
-          vy[index]       = vy[indm1];
-          vz[index]       = vz[indm1]; 
-          if(vx[index]<0.) vx[index] = 0.0;
-          GRHayLHD_enforce_primitive_limits_and_compute_conservs(cctkGH, index);
+          const int indm1 = CCTK_GFINDEX3D(cctkGH, i, j, kmax-1);
+
+          const double vtmp = vz[indm1] < 0.0 ? 0 : vz[indm1];
+          primitive_quantities prims;
+          ghl_initialize_primitives(
+                rho_b[indm1], pressure[indm1], eps[indm1],
+                vx[indm1], vy[indm1], vtmp,
+                0.0, 0.0, 0.0,
+                poison, poison, poison, &prims);
+
+          GRHayLHD_enforce_primitive_limits_and_compute_conservs(cctkGH, index, prims);
+
+          ghl_return_primitives(
+                &prims,
+                &rho_b[index], &pressure[index], &eps[index],
+                &vx[index], &vy[index], &vz[index],
+                &dummy1, &dummy2, &dummy3,
+                &dummy4, &dummy5, &dummy6);
         }
       }
     }
@@ -140,27 +192,35 @@ void GRHayLHD_outer_boundaries_on_P_rho_b_vx_vy_vz(CCTK_ARGUMENTS) {
         for(int i=0; i<cctk_lsh[0]; i++) {
           const int index = CCTK_GFINDEX3D(cctkGH, i, j, kmin);
           const int indp1 = CCTK_GFINDEX3D(cctkGH, i, j, kmin+1);
-          pressure[index] = pressure[indp1];
-          rho_b[index]    = rho_b[indp1];
-          vx[index]       = vx[indp1];
-          vy[index]       = vy[indp1];
-          vz[index]       = vz[indp1]; 
-          if(vx[index]>0.) vx[index] = 0.0;
-          GRHayLHD_enforce_primitive_limits_and_compute_conservs(cctkGH, index);
+
+          const double vtmp = vz[indp1] > 0.0 ? 0 : vz[indp1];
+          primitive_quantities prims;
+          ghl_initialize_primitives(
+                rho_b[indp1], pressure[indp1], eps[indp1],
+                vx[indp1], vy[indp1], vtmp,
+                0.0, 0.0, 0.0,
+                poison, poison, poison, &prims);
+
+          GRHayLHD_enforce_primitive_limits_and_compute_conservs(cctkGH, index, prims);
+
+          ghl_return_primitives(
+                &prims,
+                &rho_b[index], &pressure[index], &eps[index],
+                &vx[index], &vy[index], &vz[index],
+                &dummy1, &dummy2, &dummy3,
+                &dummy4, &dummy5, &dummy6);
         }
       }
     }
   }
 }
 
-void GRHayLHD_enforce_primitive_limits_and_compute_conservs(const cGH* cctkGH, const int index) {
+void GRHayLHD_enforce_primitive_limits_and_compute_conservs(const cGH* cctkGH, const int index, primitive_quantities prims) {
   // We cheat here by using the argument list of the scheduled function
   // instead of explicitly passing all these variables.
   DECLARE_CCTK_ARGUMENTS_GRHayLHD_outer_boundaries_on_P_rho_b_vx_vy_vz;
 
-  const double poison = 0.0/0.0;
-  double dummy1, dummy2, dummy3;
-  double dummy4, dummy5, dummy6;
+  double dummy1, dummy2;
   int speed_limited = 0;
 
   metric_quantities ADM_metric;
@@ -174,13 +234,6 @@ void GRHayLHD_enforce_primitive_limits_and_compute_conservs(const cGH* cctkGH, c
   ADM_aux_quantities metric_aux;
   ghl_compute_ADM_auxiliaries(&ADM_metric, &metric_aux);
 
-  primitive_quantities prims;
-  ghl_initialize_primitives(
-        rho_b[index], pressure[index], eps[index],
-        vx[index], vy[index], vz[index],
-        0.0, 0.0, 0.0,
-        poison, poison, poison, &prims);
-
   conservative_quantities cons;
   ghl_enforce_primitive_limits_and_compute_u0(
         ghl_params, ghl_eos, &ADM_metric,
@@ -188,13 +241,6 @@ void GRHayLHD_enforce_primitive_limits_and_compute_conservs(const cGH* cctkGH, c
 
   ghl_compute_conservs(
         &ADM_metric, &metric_aux, &prims, &cons);
-
-  ghl_return_primitives(
-        &prims,
-        &rho_b[index], &pressure[index], &eps[index],
-        &vx[index], &vy[index], &vz[index],
-        &dummy1, &dummy2, &dummy3,
-        &dummy4, &dummy5, &dummy6);
 
   ghl_return_conservatives(
         &cons,
