c724616acb74d9fc990e84e1995f65a8af2be6c3
Leo Werneck <wernecklr@gmail.com>
2025-02-18 21:56:18 -0800

    Added u0 gridfunction; added Prim2Con; added configuration.ccl

--- GRHayLMHD/src/GRHayLMHD.h
+++ GRHayLMHD/src/GRHayLMHD.h
@@ -8,18 +8,19 @@
 #include <math.h>
 #include <stdbool.h>
 
-#define LOOP3D(_imin, _imax, _jmin, _jmax, _kmin, _kmax)             \
-    for(int k = _kmin; k < _kmax; k++) {                             \
-        for(int j = _jmin; j < _jmax; j++) {                         \
-            for(int i = _imin; i < _imax; i++) {                     \
-                const int ijk  = CCTK_GFINDEX3D(cctkGH, i, j, k);    \
-                const int ijkx = CCTK_GFINDEX4D(cctkGH, i, j, k, 0); \
-                const int ijky = CCTK_GFINDEX4D(cctkGH, i, j, k, 1); \
-                const int ijkz = CCTK_GFINDEX4D(cctkGH, i, j, k, 2);
+#include "GRHayLib.h"
+
+#define LOOP3D(_imin, _imax, _jmin, _jmax, _kmin, _kmax)                                   \
+    for(int k = _kmin; k < _kmax; k++) {                                                   \
+        for(int j = _jmin; j < _jmax; j++) {                                               \
+            for(int i = _imin; i < _imax; i++) {                                           \
+                CCTK_ATTRIBUTE_UNUSED const int ijk  = CCTK_GFINDEX3D(cctkGH, i, j, k);    \
+                CCTK_ATTRIBUTE_UNUSED const int ijkx = CCTK_GFINDEX4D(cctkGH, i, j, k, 0); \
+                CCTK_ATTRIBUTE_UNUSED const int ijky = CCTK_GFINDEX4D(cctkGH, i, j, k, 1); \
+                CCTK_ATTRIBUTE_UNUSED const int ijkz = CCTK_GFINDEX4D(cctkGH, i, j, k, 2);
 
 #define OMPLOOP3D(_imin, _imax, _jmin, _jmax, _kmin, _kmax) \
-    _Pragma("omp parallel for")                             \
-        LOOP3D(_imin, _imax, _jmin, _jmax, _kmin, _kmax)
+    _Pragma("omp parallel for") LOOP3D(_imin, _imax, _jmin, _jmax, _kmin, _kmax)
 
 #define ENDLOOP3D \
     }             \
