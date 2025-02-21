04ef06784fc5d108e36f706cab58816eaa1cc521
Leo Werneck <wernecklr@gmail.com>
2025-02-20 19:34:50 -0800

    Minor cleanup of EM outer BCs

--- GRHayLMHD/src/Outer_Boundaries_EM_FourPotential.c
+++ GRHayLMHD/src/Outer_Boundaries_EM_FourPotential.c
@@ -1,5 +1,11 @@
 #include "GRHayLMHD.h"
 
+#define GRHAYLMHD_APPLY_EM_BCS(_ijk, _ijk_pm1, _ijk_pm2)               \
+    A_x_tilde[_ijk] = 2.0 * A_x_tilde[_ijk_pm1] - A_x_tilde[_ijk_pm2]; \
+    A_y_tilde[_ijk] = 2.0 * A_y_tilde[_ijk_pm1] - A_y_tilde[_ijk_pm2]; \
+    A_z_tilde[_ijk] = 2.0 * A_z_tilde[_ijk_pm1] - A_z_tilde[_ijk_pm2]; \
+    Phi_tilde[_ijk] = 2.0 * Phi_tilde[_ijk_pm1] - Phi_tilde[_ijk_pm2];
+
 void GRHayLMHD_Outer_Boundaries_EM_FourPotential(CCTK_ARGUMENTS)
 {
     DECLARE_CCTK_ARGUMENTS;
@@ -35,11 +41,7 @@ void GRHayLMHD_Outer_Boundaries_EM_FourPotential(CCTK_ARGUMENTS)
                     const int ijk   = CCTK_GFINDEX3D(cctkGH, imin, j, k);
                     const int ip1jk = CCTK_GFINDEX3D(cctkGH, imin + 1, j, k);
                     const int ip2jk = CCTK_GFINDEX3D(cctkGH, imin + 2, j, k);
-
-                    A_x_tilde[ijk] = 2.0 * A_x_tilde[ip1jk] - A_x_tilde[ip2jk];
-                    A_y_tilde[ijk] = 2.0 * A_y_tilde[ip1jk] - A_y_tilde[ip2jk];
-                    A_z_tilde[ijk] = 2.0 * A_z_tilde[ip1jk] - A_z_tilde[ip2jk];
-                    Phi_tilde[ijk] = 2.0 * Phi_tilde[ip1jk] - Phi_tilde[ip2jk];
+                    GRHAYLMHD_APPLY_EM_BCS(ijk, ip1jk, ip2jk);
                 }
             }
         }
@@ -50,11 +52,7 @@ void GRHayLMHD_Outer_Boundaries_EM_FourPotential(CCTK_ARGUMENTS)
                     const int ijk   = CCTK_GFINDEX3D(cctkGH, imax, j, k);
                     const int im1jk = CCTK_GFINDEX3D(cctkGH, imax - 1, j, k);
                     const int im2jk = CCTK_GFINDEX3D(cctkGH, imax - 2, j, k);
-
-                    A_x_tilde[ijk] = 2.0 * A_x_tilde[im1jk] - A_x_tilde[im2jk];
-                    A_y_tilde[ijk] = 2.0 * A_y_tilde[im1jk] - A_y_tilde[im2jk];
-                    A_z_tilde[ijk] = 2.0 * A_z_tilde[im1jk] - A_z_tilde[im2jk];
-                    Phi_tilde[ijk] = 2.0 * Phi_tilde[im1jk] - Phi_tilde[im2jk];
+                    GRHAYLMHD_APPLY_EM_BCS(ijk, im1jk, im2jk);
                 }
             }
         }
@@ -65,11 +63,7 @@ void GRHayLMHD_Outer_Boundaries_EM_FourPotential(CCTK_ARGUMENTS)
                     const int ijk   = CCTK_GFINDEX3D(cctkGH, i, jmin, k);
                     const int ijp1k = CCTK_GFINDEX3D(cctkGH, i, jmin + 1, k);
                     const int ijp2k = CCTK_GFINDEX3D(cctkGH, i, jmin + 2, k);
-
-                    A_x_tilde[ijk] = 2.0 * A_x_tilde[ijp1k] - A_x_tilde[ijp2k];
-                    A_y_tilde[ijk] = 2.0 * A_y_tilde[ijp1k] - A_y_tilde[ijp2k];
-                    A_z_tilde[ijk] = 2.0 * A_z_tilde[ijp1k] - A_z_tilde[ijp2k];
-                    Phi_tilde[ijk] = 2.0 * Phi_tilde[ijp1k] - Phi_tilde[ijp2k];
+                    GRHAYLMHD_APPLY_EM_BCS(ijk, ijp1k, ijp2k);
                 }
             }
         }
@@ -80,11 +74,7 @@ void GRHayLMHD_Outer_Boundaries_EM_FourPotential(CCTK_ARGUMENTS)
                     const int ijk   = CCTK_GFINDEX3D(cctkGH, i, jmax, k);
                     const int ijm1k = CCTK_GFINDEX3D(cctkGH, i, jmax - 1, k);
                     const int ijm2k = CCTK_GFINDEX3D(cctkGH, i, jmax - 2, k);
-
-                    A_x_tilde[ijk] = 2.0 * A_x_tilde[ijm1k] - A_x_tilde[ijm2k];
-                    A_y_tilde[ijk] = 2.0 * A_y_tilde[ijm1k] - A_y_tilde[ijm2k];
-                    A_z_tilde[ijk] = 2.0 * A_z_tilde[ijm1k] - A_z_tilde[ijm2k];
-                    Phi_tilde[ijk] = 2.0 * Phi_tilde[ijm1k] - Phi_tilde[ijm2k];
+                    GRHAYLMHD_APPLY_EM_BCS(ijk, ijm1k, ijm2k);
                 }
             }
         }
@@ -95,11 +85,7 @@ void GRHayLMHD_Outer_Boundaries_EM_FourPotential(CCTK_ARGUMENTS)
                     const int ijk   = CCTK_GFINDEX3D(cctkGH, i, j, kmin);
                     const int ijkp1 = CCTK_GFINDEX3D(cctkGH, i, j, kmin + 1);
                     const int ijkp2 = CCTK_GFINDEX3D(cctkGH, i, j, kmin + 2);
-
-                    A_x_tilde[ijk] = 2.0 * A_x_tilde[ijkp1] - A_x_tilde[ijkp2];
-                    A_y_tilde[ijk] = 2.0 * A_y_tilde[ijkp1] - A_y_tilde[ijkp2];
-                    A_z_tilde[ijk] = 2.0 * A_z_tilde[ijkp1] - A_z_tilde[ijkp2];
-                    Phi_tilde[ijk] = 2.0 * Phi_tilde[ijkp1] - Phi_tilde[ijkp2];
+                    GRHAYLMHD_APPLY_EM_BCS(ijk, ijkp1, ijkp2);
                 }
             }
         }
@@ -110,11 +96,7 @@ void GRHayLMHD_Outer_Boundaries_EM_FourPotential(CCTK_ARGUMENTS)
                     const int ijk   = CCTK_GFINDEX3D(cctkGH, i, j, kmax);
                     const int ijkm1 = CCTK_GFINDEX3D(cctkGH, i, j, kmax - 1);
                     const int ijkm2 = CCTK_GFINDEX3D(cctkGH, i, j, kmax - 2);
-
-                    A_x_tilde[ijk] = 2.0 * A_x_tilde[ijkm1] - A_x_tilde[ijkm2];
-                    A_y_tilde[ijk] = 2.0 * A_y_tilde[ijkm1] - A_y_tilde[ijkm2];
-                    A_z_tilde[ijk] = 2.0 * A_z_tilde[ijkm1] - A_z_tilde[ijkm2];
-                    Phi_tilde[ijk] = 2.0 * Phi_tilde[ijkm1] - Phi_tilde[ijkm2];
+                    GRHAYLMHD_APPLY_EM_BCS(ijk, ijkm1, ijkm2);
                 }
             }
         }
