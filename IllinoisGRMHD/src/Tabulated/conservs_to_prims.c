67c52a633ef99e938f0b043d2e329b6b2a87b941
Leonardo Werneck <leow155@gmail.com>
2025-04-17 19:01:11 -0700

    Fixed major (but silly) issue affecting all but Hybrid con2prim. (#14)

--- IllinoisGRMHD/src/Tabulated/conservs_to_prims.c
+++ IllinoisGRMHD/src/Tabulated/conservs_to_prims.c
@@ -91,7 +91,7 @@ void IllinoisGRMHD_tabulated_conservs_to_prims(CCTK_ARGUMENTS) {
                 &cons_undens, &prims, &diagnostics);
 
           if(isnan(prims.rho*prims.press*prims.eps*prims.vU[0]*prims.vU[1]*prims.vU[2]*
-                            prims.Y_e*prims.temperature)) {
+                            prims.Y_e*prims.temperature))
             error = ghl_error_c2p_singular;
         } else {
           ghl_set_prims_to_constant_atm(ghl_eos, &prims);
@@ -100,6 +100,7 @@ void IllinoisGRMHD_tabulated_conservs_to_prims(CCTK_ARGUMENTS) {
           error = ghl_success;
         }
 
+        if(error) {
           pointcount_avg++;
 
           ghl_conservative_quantities cons_neigh_avg, cons_avg;
