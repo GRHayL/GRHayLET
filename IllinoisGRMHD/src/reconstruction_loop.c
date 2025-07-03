0df4cfac7c961d9dcb3dc128166686bddc1cc255
Leo Werneck <wernecklr@gmail.com>
2025-07-03 15:07:15 -0700

    Fixed reconstruction_loop.c to use wenoz

--- IllinoisGRMHD/src/reconstruction_loop.c
+++ IllinoisGRMHD/src/reconstruction_loop.c
@@ -26,24 +26,20 @@ void IllinoisGRMHD_reconstruction_loop(const cGH *restrict cctkGH, const int flu
     for(int j=jmin; j<jmax; j++) {
       for(int i=imin; i<imax; i++) {
         const int index = CCTK_GFINDEX3D(cctkGH, i, j, k);
-        CCTK_REAL press_stencil[6], v_flux_stencil[6];
+        CCTK_REAL v_flux_stencil[6];
         CCTK_REAL var_data[num_vars][6], vars_r[num_vars], vars_l[num_vars];
 
         for(int ind=0; ind<6; ind++) {
           // Stencil from -3 to +2 reconstructs to e.g. i-1/2
           const int stencil = CCTK_GFINDEX3D(cctkGH, i+xdir*(ind-3), j+ydir*(ind-3), k+zdir*(ind-3));
           v_flux_stencil[ind] = v_flux[stencil]; // Could be smaller; doesn't use full stencil
-          press_stencil[ind] = pressure[stencil];
           for(int var=0; var<num_vars; var++) {
             var_data[var][ind] = in_prims[var_indices[var]][stencil];
           }
         }
 
-        CCTK_REAL ftilde[2];
-        ghl_compute_ftilde(ghl_params, press_stencil, v_flux_stencil, ftilde);
-
         for(int var=0; var<num_vars; var++) {
-          ghl_ppm_reconstruction(ftilde, var_data[var], &vars_r[var], &vars_l[var]);
+          ghl_wenoz_reconstruction(var_data[var], &vars_r[var], &vars_l[var]);
           out_prims_r[var_indices[var]][index] = vars_r[var];
           out_prims_l[var_indices[var]][index] = vars_l[var];
         }
